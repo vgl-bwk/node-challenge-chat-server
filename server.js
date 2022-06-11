e8d68c88915a076bcd2bf18f61bf2b0e2efcd098
vgl-bwk <83692259+vgl-bwk@users.noreply.github.com>
2022-06-11 12:05:06 +0200

    Completed levels 1 & 2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const res = require("express/lib/response");
 
 const app = express();
 
@@ -14,12 +15,37 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
+app.use(express.json());
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/messages", function (req, res) {
+  res.send(messages);
+});
+app.get("/messages/:id", function (req, res) {
+  const messageId = Number(req.params.id);
+  const message = messages.find((m) => m.id === messageId);
+  res.send(message);
+});
+
+app.post("/messages", function (req, res) {
+  if (!req.body.text || !req.body.from) {
+    return res.status(400).send();
+  }
+  const newMessage = req.body;
+  const newID = messages.length;
+  newMessage.id = newID;
+  messages.push(newMessage);
+  res.send(newMessage);
+});
+app.delete("/messages/:id", function (req, res) {
+  const messageId = Number(req.params.id);
+  messages = messages.filter((m) => m.id != messageId);
+  res.send("Message deleted");
 });
 
 app.listen(3000, () => {
-   console.log("Listening on port 3000")
-  });
+  console.log("Listening on port 3000");
+});
